efd3540f230fe215eb70783567536f96e83ee6a7
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-07 11:30:38 +0530

    v2

--- app.js
+++ app.js
@@ -1,15 +1,109 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const session = require("express-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/user-profiles", {useNewUrlParser: true});
+
+const userSechema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+});
+
+userSechema.plugin(passportLocalMongoose);
+userSechema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSechema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
 app.get("/", (req, res)=>{
     res.render("login");
 });
+
+app.get("/home", (req, res)=>{
+    User.find({"secret": {$ne: null}}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            if(foundUser){
+                res.render("index", {userWithSecrets: foundUser});
+            }
+        }
+    });
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+app.get("/:routs", (req, res)=>{
+    const goto = req.params.routs;
+    res.render(goto);
+});
+
+app.post("/login", function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    });
+
+    req.login(user, function(err){
+        if(!err){
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/index");
+            });
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+
+app.post("/register", function(req, res){
+    User.register({username: req.body.username, email: req.body.email}, req.body.password, function(err, user){
+        if(!err){
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/index");
+            });
+        } else {
+            console.log(err);
+            res.redirect("/login");
+        }
+    });
+});
+
 
 
 const port = process.env.PORT || 3000;
--- public/js/login.js
+++ public/js/login.js
@@ -9,3 +9,56 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+// form validation
+function clearErrors(){
+
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
+
+
+}
+function seterror(id, error){
+    //sets error inside tag of id 
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+
+}
+
+function validateForm(){
+    var returnval = true;
+    clearErrors();
+    var name = document.forms['myForm']["username"].value;
+    if (name.length<3){
+        seterror("name", "*Length of name is too short");
+        returnval = false;
+    }
+
+    if (name.length >= 25){
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+
+    var email = document.forms['myForm']["email"].value;
+    if (email.length>20){
+        seterror("email", "*Email length is too long");
+        returnval = false;
+    }
+
+    var password = document.forms['myForm']["password"].value;
+    if (password.length < 6){
+        seterror("pass", "*Password should be atleast 6 characters long!");
+        returnval = false;
+    }
+
+    var cpassword = document.forms['myForm']["cpass"].value;
+    if (cpassword != password){
+        seterror("cpass", "*Password and Confirm password should match!");
+        returnval = false;
+    }
+
+    return returnval;
+}
